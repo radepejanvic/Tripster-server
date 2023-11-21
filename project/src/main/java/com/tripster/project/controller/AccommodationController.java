fd98443e76dca0112b7285cb066244c90e9c9c05
Rade Pejanović <radepejanovic@gmail.com>
2023-11-21 22:48:53 +0100

    feat: Post accommodation request

--- project/src/main/java/com/tripster/project/controller/AccommodationController.java
+++ project/src/main/java/com/tripster/project/controller/AccommodationController.java
@@ -5,10 +5,7 @@ import com.tripster.project.service.AccommodationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +34,12 @@ public class AccommodationController {
         }
 
         return new ResponseEntity<>(accommodation, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<Accommodation> saveCourse(@RequestBody Accommodation accommodation) {
+        accommodation = accommodationService.save(accommodation);
+        return new ResponseEntity<>(accommodation, HttpStatus.CREATED);
     }
 
 }
