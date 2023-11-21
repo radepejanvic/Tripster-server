6923827596a3590b9024653579ba158e949cac9f
Rade Pejanović <radepejanovic@gmail.com>
2023-11-21 22:46:46 +0100

    feat: Get one accommodations request

--- project/src/main/java/com/tripster/project/controller/AccommodationController.java
+++ project/src/main/java/com/tripster/project/controller/AccommodationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,17 @@ public class AccommodationController {
         return new ResponseEntity<>(accommodations, HttpStatus.OK);
     }
 
-    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Accommodation> getCourse(@PathVariable Long id) {
+
+        Accommodation accommodation = accommodationService.findOne(id);
+
+        // course must exist
+        if (accommodation == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(accommodation, HttpStatus.OK);
+    }
 
 }
