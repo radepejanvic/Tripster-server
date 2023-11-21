bf1a3eea0cce9c112f9fd8de24c27d44e9f5db06
Rade Pejanović <radepejanovic@gmail.com>
2023-11-21 21:49:46 +0100

    feat: Initialized Accommodation service

---
+++ project/src/main/java/com/tripster/project/service/AccommodationService.java
@@ -0,0 +1,6 @@
+package com.tripster.project.service;
+
+import com.tripster.project.repository.IAccommodationRepository;
+
+public class AccommodationService implements IAccommodationRepository {
+}
