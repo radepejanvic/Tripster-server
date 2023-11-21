f605e305cb9e5991303b3098c625a8be647bb765
Rade Pejanović <radepejanovic@gmail.com>
2023-11-21 22:30:55 +0100

    feat: Created AccommodationService

--- project/src/main/java/com/tripster/project/service/AccommodationService.java
+++ project/src/main/java/com/tripster/project/service/AccommodationService.java
@@ -1,6 +1,37 @@
 package com.tripster.project.service;
 
-import com.tripster.project.repository.IAccommodationRepository;
+import com.tripster.project.model.Accommodation;
+import com.tripster.project.repository.AccommodationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-public class AccommodationService implements IAccommodationRepository {
+import java.util.List;
+
+@Service
+public class AccommodationService {
+
+    @Autowired
+    private AccommodationRepository accommodationRepository;
+
+    public Accommodation findOne(Integer id) {
+        return accommodationRepository.findById(id).orElseGet(null);
+    }
+
+    public List<Accommodation> findAll() {
+        return accommodationRepository.findAll();
+    }
+
+    public Page<Accommodation> findAll(Pageable page) {
+        return accommodationRepository.findAll(page);
+    }
+
+    public Accommodation save(Accommodation Accommodation) {
+        return accommodationRepository.save(Accommodation);
+    }
+
+    public void remove(Integer id) {
+        accommodationRepository.deleteById(id);
+    }
 }
