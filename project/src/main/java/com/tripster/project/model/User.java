bff7934df7167572bd4630dc51594c7d88c0c4cc
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-11-22 15:13:27 +0100

    fix: Added tag table name "users" in class User

--- project/src/main/java/com/tripster/project/model/User.java
+++ project/src/main/java/com/tripster/project/model/User.java
@@ -4,6 +4,7 @@ import com.tripster.project.model.enums.UserStatus;
 import com.tripster.project.model.enums.UserType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
