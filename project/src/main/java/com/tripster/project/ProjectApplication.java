924003d18c48071a3f1c18e2015dd484f8ad802b
Rade Pejanović <radepejanovic@gmail.com>
2023-11-24 21:01:20 +0100

    feat: Added ModelMapper as a bean

--- project/src/main/java/com/tripster/project/ProjectApplication.java
+++ project/src/main/java/com/tripster/project/ProjectApplication.java
@@ -1,11 +1,17 @@
 package com.tripster.project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectApplication {
 
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
