7a2d69473d028f07d14e7c3994712657f75fd986
Rade Pejanović <radepejanovic@gmail.com>
2023-11-25 12:13:21 +0100

    feat: Created role specific accommodation mappers

--- project/src/main/java/com/tripster/project/mapper/AccommodationDTOMapper.java
+++ project/src/main/java/com/tripster/project/mapper/AccommodationDTOMapper.java
@@ -1,5 +1,8 @@
 package com.tripster.project.mapper;
 
+import com.tripster.project.dto.AccommodationCardAdminDTO;
+import com.tripster.project.dto.AccommodationCardGuestDTO;
+import com.tripster.project.dto.AccommodationCardHostDTO;
 import com.tripster.project.dto.AccommodationDTO;
 import com.tripster.project.model.Accommodation;
 import org.modelmapper.ModelMapper;
@@ -20,8 +23,20 @@ public class AccommodationDTOMapper {
         return modelMapper.map(dto, Accommodation.class);
     }
 
-    public static AccommodationDTO fromAccommodationToDTO(Accommodation dto) {
-        return modelMapper.map(dto, AccommodationDTO.class);
+    public static AccommodationDTO fromAccommodationToDTO(Accommodation accommodation) {
+        return modelMapper.map(accommodation, AccommodationDTO.class);
+    }
+
+    public static AccommodationCardAdminDTO fromAccommodationToAdminDTO(Accommodation accommodation) {
+        return modelMapper.map(accommodation, AccommodationCardAdminDTO.class);
+    }
+
+    public static AccommodationCardHostDTO fromAccommodationToHostDTO(Accommodation accommodation) {
+        return modelMapper.map(accommodation, AccommodationCardHostDTO.class);
+    }
+
+    public static AccommodationCardGuestDTO fromAccommodationToGuestDTO(Accommodation accommodation) {
+        return modelMapper.map(accommodation, AccommodationCardGuestDTO.class);
     }
 }
 
