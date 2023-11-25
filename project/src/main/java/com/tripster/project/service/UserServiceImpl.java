3f68ed2df31da95a2a06ac2a4664d2a231c3b1a2
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-11-25 23:59:48 +0100

    feat: Added findByEmailAndPassword

--- project/src/main/java/com/tripster/project/service/UserServiceImpl.java
+++ project/src/main/java/com/tripster/project/service/UserServiceImpl.java
@@ -1,7 +1,14 @@
 package com.tripster.project.service;
 
+import com.tripster.project.model.Address;
+import com.tripster.project.model.Person;
 import com.tripster.project.model.User;
+import com.tripster.project.model.enums.UserStatus;
+import com.tripster.project.model.enums.UserType;
+import com.tripster.project.repository.AddressRepository;
+import com.tripster.project.repository.PersonRepository;
 import com.tripster.project.repository.UserRepository;
+import com.tripster.project.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,10 +17,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserServiceImpl implements IUserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private PersonRepository personRepository;
 
     public User findOne(Long id) {
         return userRepository.findById(id).orElseGet(null);
@@ -33,6 +44,12 @@ public class UserService {
 
     public void remove(Long id) {
         userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findByEmailAndPassword(String email, String password) {
+        User user = userRepository.findByEmailAndPassword(email,password);
+        return user;
     }
 
 }
