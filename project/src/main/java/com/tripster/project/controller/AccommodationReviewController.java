3247c8d433becfac374903fac4017a0d3b3e5a71
Rade Pejanović <radepejanovic@gmail.com>
2023-11-26 22:10:09 +0100

    feat: Created Post mapping

--- project/src/main/java/com/tripster/project/controller/AccommodationReviewController.java
+++ project/src/main/java/com/tripster/project/controller/AccommodationReviewController.java
@@ -1,19 +1,21 @@
 package com.tripster.project.controller;
 
 import com.tripster.project.dto.AccommodationCardAdminDTO;
+import com.tripster.project.dto.AccommodationDTO;
 import com.tripster.project.dto.AccommodationReviewDTO;
 import com.tripster.project.mapper.AccommodationDTOMapper;
 import com.tripster.project.mapper.AccommodationReviewDTOMapper;
 import com.tripster.project.model.Accommodation;
 import com.tripster.project.model.AccommodationReview;
+import com.tripster.project.model.Host;
 import com.tripster.project.service.AccommodationReviewService;
+import com.tripster.project.service.interfaces.IPersonService;
+import com.tripster.project.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +26,8 @@ public class AccommodationReviewController {
 
     @Autowired
     private AccommodationReviewService accommodationReviewService;
+    @Autowired
+    private IUserService userService;
 
     @GetMapping(value = "/{accommodationId}")
     public ResponseEntity<List<AccommodationReviewDTO>> getReviews(@PathVariable Long accommodationId) {
@@ -35,6 +39,18 @@ public class AccommodationReviewController {
 
         return new ResponseEntity<>(dtos, HttpStatus.OK);
     }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<AccommodationReviewDTO> saveAccommodation(@RequestBody AccommodationReviewDTO dto) {
+
+        AccommodationReview review = AccommodationReviewDTOMapper.fromDTOToAccommodationReview(dto);
+        review.setReviewer(userService.findOne(dto.getReviewerId()));
+        accommodationReviewService.save(review);
+
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+
 
 
 }
