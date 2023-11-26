36f6d869d52024273c0f401dacd10d46edae481f
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-11-26 17:01:42 +0100

    feat: Added function findAll

--- project/src/main/java/com/tripster/project/service/interfaces/IPersonService.java
+++ project/src/main/java/com/tripster/project/service/interfaces/IPersonService.java
@@ -2,14 +2,19 @@ package com.tripster.project.service.interfaces;
 
 import com.tripster.project.model.Person;
 import com.tripster.project.model.User;
-import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 
 public interface IPersonService {
     public Person save(Person person);
 
     public Person findByUser(User user);
+    public List<Person> findAll();
 
+    public Page<Person> findAll(Pageable page);
     public Person findById(Long id);
     public void remove(Long id);
 }
