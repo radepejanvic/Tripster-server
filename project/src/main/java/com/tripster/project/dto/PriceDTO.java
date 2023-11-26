db587ef967fd1896431879646057405954ce606d
Rade Pejanović <radepejanovic@gmail.com>
2023-11-26 20:10:53 +0100

    feat: Created PriceDTO

---
+++ project/src/main/java/com/tripster/project/dto/PriceDTO.java
@@ -0,0 +1,18 @@
+package com.tripster.project.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PriceDTO {
+
+    private LocalDate start;
+    private LocalDate end;
+    private double price;
+    
+}
