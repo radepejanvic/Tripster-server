dcce3806890c55092b08f8b0c05ea8579b6b3f2c
Rade Pejanović <radepejanovic@gmail.com>
2023-11-26 23:00:26 +0100

    feat: Added findAllByReviedId

--- project/src/main/java/com/tripster/project/repository/UserReviewRepository.java
+++ project/src/main/java/com/tripster/project/repository/UserReviewRepository.java
@@ -2,6 +2,14 @@ package com.tripster.project.repository;
 
 import com.tripster.project.model.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
+
+    @Query("select r from UserReview r " +
+            "join fetch r.reviewedUser u " +
+            "where u.id = :reviewdId")
+    List<UserReview> findAllByReviewedId(Long reviewedId);
 }
