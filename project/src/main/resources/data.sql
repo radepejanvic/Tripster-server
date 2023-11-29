eb70b91d7f659b9f503a3de743f6f6f917c568cd
Rade Pejanović <radepejanovic@gmail.com>
2023-11-29 15:46:25 +0100

    fix: Resolved bugs

--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -31,21 +31,21 @@ insert into host (name, surname, phone, address_id, user_id) values ('Olenka', '
 insert into host (name, surname, phone, address_id, user_id) values ('Beverlee', 'Potkins', '848-215-6921', 8, 8);
 insert into host (name, surname, phone, address_id, user_id) values ('Bobbee', 'January', '104-170-4066', 9, 9);
 
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Reilly-Volkman', 6, 'Burn of first degree of toe(s) (nail)', 'Revision of Autologous Tissue Substitute in Right Ear, Percutaneous Approach', 1, 'STUDIO', 'ACTIVE', 1, 3, 10, false);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Fisher and Sons', 6, 'Melanocytic nevi of unspecified lower limb, including hip', 'Removal of Spacer from Right Carpal Joint, Open Approach', 2, 'APARTMENT', 'ACTIVE', 1, 3, 20, false);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Pfeffer-Schmeler', 6, 'Corrosion of unsp degree of unspecified forearm, init encntr', 'Excision of Left Lower Lobe Bronchus, Percutaneous Endoscopic Approach, Diagnostic', 3, 'ROOM', 'ACTIVE', 1, 3, 30, true);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('McCullough-Zieme', 6, 'Laceration with foreign body of forearm', 'Drainage of Descending Colon, Via Natural or Artificial Opening Endoscopic, Diagnostic', 4, 'ROOM', 'ACTIVE', 2, 5, 14, true);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Lehner Group', 6, 'Milt op w indirect blast effect of nuclear weapon, civ, init', 'Introduction of Anti-inflammatory into Cranial Nerves, Percutaneous Approach', 5, 'STUDIO', 'ACTIVE', 2, 5, 15, false);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Hand and Sons', 7, 'Benign lipomatous neoplasm of intra-abdominal organs', 'Replacement of Left Choroid with Autologous Tissue Substitute, Percutaneous Approach', 6, 'ROOM', 'ACTIVE', 0, 6, 16, false);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Predovic-Orn', 7, 'Sarcoidosis, unspecified', 'Extirpation of Matter from Portal Vein, Open Approach', 7, 'ROOM', 'ACTIVE', 0, 6, 17, true);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Lehner, Schiller and Dickinson', 7, 'Dislocation of tarsal joint of unspecified foot', 'Fragmentation in Accessory Pancreatic Duct, Via Natural or Artificial Opening Endoscopic', 8, 'STUDIO', 'ACTIVE', 0, 8, 18, false);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Blanda, Herzog and Rohan', 8, 'Displ oblique fx shaft of l ulna, 7thR', 'Introduction of Radioactive Substance into Central Vein, Percutaneous Approach', 9, 'ROOM', 'ACTIVE', 1, 9, 19, true);
-insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Gleichner, Toy and Stracke', 8, 'Poisoning by antiparkns drug/centr muscle-tone depr, undet', 'Control Bleeding in Upper Back, Percutaneous Endoscopic Approach', 10, 'STUDIO', 'ACTIVE', 7, 10, 10, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Reilly-Volkman', 1, 'Burn of first degree of toe(s) (nail)', 'Revision of Autologous Tissue Substitute in Right Ear, Percutaneous Approach', 1, 'STUDIO', 'ACTIVE', 1, 3, 10, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Fisher and Sons', 1, 'Melanocytic nevi of unspecified lower limb, including hip', 'Removal of Spacer from Right Carpal Joint, Open Approach', 2, 'APARTMENT', 'ACTIVE', 1, 3, 20, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Pfeffer-Schmeler', 1, 'Corrosion of unsp degree of unspecified forearm, init encntr', 'Excision of Left Lower Lobe Bronchus, Percutaneous Endoscopic Approach, Diagnostic', 3, 'ROOM', 'ACTIVE', 1, 3, 30, true);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('McCullough-Zieme', 1, 'Laceration with foreign body of forearm', 'Drainage of Descending Colon, Via Natural or Artificial Opening Endoscopic, Diagnostic', 4, 'ROOM', 'ACTIVE', 2, 5, 14, true);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Lehner Group', 1, 'Milt op w indirect blast effect of nuclear weapon, civ, init', 'Introduction of Anti-inflammatory into Cranial Nerves, Percutaneous Approach', 5, 'STUDIO', 'ACTIVE', 2, 5, 15, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Hand and Sons', 2, 'Benign lipomatous neoplasm of intra-abdominal organs', 'Replacement of Left Choroid with Autologous Tissue Substitute, Percutaneous Approach', 6, 'ROOM', 'ACTIVE', 0, 6, 16, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Predovic-Orn', 2, 'Sarcoidosis, unspecified', 'Extirpation of Matter from Portal Vein, Open Approach', 7, 'ROOM', 'ACTIVE', 0, 6, 17, true);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Lehner, Schiller and Dickinson', 2, 'Dislocation of tarsal joint of unspecified foot', 'Fragmentation in Accessory Pancreatic Duct, Via Natural or Artificial Opening Endoscopic', 8, 'STUDIO', 'ACTIVE', 0, 8, 18, false);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Blanda, Herzog and Rohan', 3, 'Displ oblique fx shaft of l ulna, 7thR', 'Introduction of Radioactive Substance into Central Vein, Percutaneous Approach', 9, 'ROOM', 'ACTIVE', 1, 9, 19, true);
+insert into accommodation (name, owner_id, short_description, description, address_id, type, status, min_cap, max_cap, cancel_duration, automatic_reservation) values ('Gleichner, Toy and Stracke', 3, 'Poisoning by antiparkns drug/centr muscle-tone depr, undet', 'Control Bleeding in Upper Back, Percutaneous Endoscopic Approach', 10, 'STUDIO', 'ACTIVE', 7, 10, 10, false);
 
-insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('22/07/2023', '26/07/2023', 4, 120.0, 2, 1, 1, 'CANCELLED');
-insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('03/12/2022', '09/12/2022', 6, 240.0, 2, 2, 1, 'CANCELLED');
-insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('13/08/2023', '14/08/2023', 1, 30.0, 3, 3, 2, 'PENDING');
-insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('12/08/2023', '17/08/2023', 5, 100.0, 4, 4, 3, 'PENDING');
+insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('2023-07-22', '2023-07-26', 4, 120.0, 2, 1, 1, 'CANCELLED');
+insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('2022-12-03', '2022-12-09', 6, 240.0, 2, 2, 1, 'CANCELLED');
+insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('2023-08-13', '2023-08-14', 1, 30.0, 3, 3, 2, 'PENDING');
+insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status) values ('2023-08-12', '2023-08-17', 5, 100.0, 4, 4, 3, 'PENDING');
 
 insert into notification (user_id, text, status) values (1, 'Exc of accessory spleen', 'SENT');
 insert into notification (user_id, text, status) values (1, 'Remove penetrat FB eye', 'READ');
@@ -58,22 +58,23 @@ insert into notification (user_id, text, status) values (5, 'Failed forceps', 'N
 insert into notification (user_id, text, status) values (5, 'Disarticulation of wrist', 'SENT');
 insert into notification (user_id, text, status) values (6, 'Ligate thoracic duct', 'SENT');
 
-insert into user_review (reviewer_id, reviewed_user_id, text, rate, status) values (1, 6, 'Incise cerebral meninges', 1, 'REPORTED');
-insert into user_review (reviewer_id, reviewed_user_id, text, rate, status) values (2, 6, 'Extracorpor hepat Assis', 2, 'REPORTED');
-insert into user_review (reviewer_id, reviewed_user_id, text, rate, status) values (3, 7, 'Total esophagectomy', 3, 'REPORTED');
-insert into user_review (reviewer_id, reviewed_user_id, text, rate, status) values (4, 8, 'Manual rupt joint adhes', 4, 'ACTIVE');
-insert into user_review (reviewer_id, reviewed_user_id, text, rate, status) values (5, 9, 'Periph ganglionect NEC', 5, 'REPORTED');
+insert into user_review (reviewer_id, reviewed_user_id, comment, rate, status) values (1, 2, 'Incise cerebral meninges', 1, 'REPORTED');
+insert into user_review (reviewer_id, reviewed_user_id, comment, rate, status) values (2, 1, 'Extracorpor hepat Assis', 2, 'REPORTED');
+insert into user_review (reviewer_id, reviewed_user_id, comment, rate, status) values (3, 2, 'Total esophagectomy', 3, 'REPORTED');
+insert into user_review (reviewer_id, reviewed_user_id, comment, rate, status) values (4, 3, 'Manual rupt joint adhes', 4, 'ACTIVE');
+insert into user_review (reviewer_id, reviewed_user_id, comment, rate, status) values (5, 4, 'Periph ganglionect NEC', 5, 'REPORTED');
 
-insert into accommodation_review (reviewer_id, accommodation_id, text, rate, status) values (1, 1, 'Wide excision of lip les', 1, 'DELETED');
-insert into accommodation_review (reviewer_id, accommodation_id, text, rate, status) values (2, 1, 'Breast dx procedure NEC', 2, 'DELETED');
-insert into accommodation_review (reviewer_id, accommodation_id, text, rate, status) values (3, 1, 'Tonometry', 3, 'ACTIVE');
-insert into accommodation_review (reviewer_id, accommodation_id, text, rate, status) values (4, 2, 'Periph nerve div NEC', 4, 'ACTIVE');
-insert into accommodation_review (reviewer_id, accommodation_id, text, rate, status) values (5, 3, 'Thymectomy NOS', 5, 'ACTIVE');
+insert into accommodation_review (reviewer_id, accommodation_id, comment, rate, status) values (1, 1, 'Wide excision of lip les', 1, 'DELETED');
+insert into accommodation_review (reviewer_id, accommodation_id, comment, rate, status) values (2, 1, 'Breast dx procedure NEC', 2, 'DELETED');
+insert into accommodation_review (reviewer_id, accommodation_id, comment, rate, status) values (3, 1, 'Tonometry', 3, 'ACTIVE');
+insert into accommodation_review (reviewer_id, accommodation_id, comment, rate, status) values (4, 2, 'Periph nerve div NEC', 4, 'ACTIVE');
+insert into accommodation_review (reviewer_id, accommodation_id, comment, rate, status) values (5, 3, 'Thymectomy NOS', 5, 'ACTIVE');
 
-insert into user_report (reporter_id, reportee_id, reason) values (6, 1, 'Sutur capsul/ligamen arm');
-insert into user_report (reporter_id, reportee_id, reason) values (6, 2, 'Mouth biopsy NOS');
-insert into user_report (reporter_id, reportee_id, reason) values (7, 1, 'Tot abd colectmy NEC/NOS');
-insert into user_report (reporter_id, reportee_id, reason) values (8, 4, 'Simple suture of dura');
-insert into user_report (reporter_id, reportee_id, reason) values (9, 5, 'Intrcoronry thromb infus');
+insert into user_report (reporter_id, reportee_id, reason) values (1, 1, 'Sutur capsul/ligamen arm');
+insert into user_report (reporter_id, reportee_id, reason) values (1, 2, 'Mouth biopsy NOS');
+insert into user_report (reporter_id, reportee_id, reason) values (2, 1, 'Tot abd colectmy NEC/NOS');
+insert into user_report (reporter_id, reportee_id, reason) values (3, 4, 'Simple suture of dura');
+insert into user_report (reporter_id, reportee_id, reason) values (2, 5, 'Intrcoronry thromb infus');
+
 
 
