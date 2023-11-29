2b2eb9f3e4466fddcac03285190741909214537c
Rade Pejanović <radepejanovic@gmail.com>
2023-11-29 12:24:13 +0100

    feat: Added inserts to Users table

--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -8,3 +8,15 @@ Insert Into address (city, country, number, street, zip_code) Values ('North Mad
 Insert Into address (city, country, number, street, zip_code) Values ('New Pascale', 'United States Minor Outlying Islands', '8236', '16643 Karen Shores', '5232');
 Insert Into address (city, country, number, street, zip_code) Values ('Conroe', 'Costa Rica', '968', '44415 Connelly Manor', '6532');
 Insert Into address (city, country, number, street, zip_code) Values ('McClureberg', 'Jordan', '84709', '2855 Daniel Ranch', '0738');
+
+
+Insert Into users (email, password, status, user_type) Values ('Holly.Lindgren@yahoo.com', '0NIi3giZFdoKjaY', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Ivan_Bogisich30@yahoo.com', 'JR78k9H69z5YzW_', 1, 0);
+Insert Into users (email, password, status, user_type) Values ('Judy_Gusikowski39@yahoo.com', 'to63t1QNRJLKIzl', 1, 0);
+Insert Into users (email, password, status, user_type) Values ('Kent83@gmail.com', 'ZXAKlKmJyxn3c_G', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Richard29@hotmail.com', 'ww6kPYJrr0d1A8D', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Douglas_Hackett51@yahoo.com', 'yKN6hINVha6fV3l', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Tammy.Nicolas88@gmail.com', 'fVzIvtOI4qM98UW', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Daniel.Orn6@gmail.com', 'bEAVKuU_KlNeYdi', 1, 0);
+Insert Into users (email, password, status, user_type) Values ('Clark69@gmail.com', 'k1EDPX5NHIJrQ68', 1, 1);
+Insert Into users (email, password, status, user_type) Values ('Marilyn_Bogisich57@hotmail.com', 'GZj6mPX3VlfZcmU', 1, 1);
