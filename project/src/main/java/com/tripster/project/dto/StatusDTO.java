24483b951afa66379a0bd595b52dc8eadf0feec1
Rade Pejanović <radepejanovic@gmail.com>
2023-11-29 18:47:09 +0100

    feat: Created StatusDTO

---
+++ project/src/main/java/com/tripster/project/dto/StatusDTO.java
@@ -0,0 +1,13 @@
+package com.tripster.project.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StatusDTO {
+    private Long id;
+    private String status;
+}
