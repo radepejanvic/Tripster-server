e378aa1f415109586cc11c3840df2c57a4ebca71
Rade Pejanović <radepejanovic@gmail.com>
2023-11-29 12:16:24 +0100

    feat: Added inserts to Address table

---
+++ project/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+Insert Into address (city, country, number, street, zip_code) Values ('Colliershire', 'Liechtenstein', '717', '245 Pagac Curve', '7977');
+Insert Into address (city, country, number, street, zip_code) Values ('South Hazel', 'Taiwan', '121', '961 Mateo Key', '7924');
+Insert Into address (city, country, number, street, zip_code) Values ('East Rosinashire', 'Benin', '28230', '24993 Ratke Gateway', '8005');
+Insert Into address (city, country, number, street, zip_code) Values ('Serenitymouth', 'Cambodia', '98718', '429 Toy Meadow', '6636');
+Insert Into address (city, country, number, street, zip_code) Values ('Niagara Falls', 'Heard Island and McDonald Islands', '401', '32583 Strosin Run', '5185');
+Insert Into address (city, country, number, street, zip_code) Values ('North Jarrettport', 'Mongolia', '8515', '57250 Noemy Gardens', '5181');
+Insert Into address (city, country, number, street, zip_code) Values ('North Madelynn', 'North Macedonia', '8652', '2248 Yost Hollow', '7558');
+Insert Into address (city, country, number, street, zip_code) Values ('New Pascale', 'United States Minor Outlying Islands', '8236', '16643 Karen Shores', '5232');
+Insert Into address (city, country, number, street, zip_code) Values ('Conroe', 'Costa Rica', '968', '44415 Connelly Manor', '6532');
+Insert Into address (city, country, number, street, zip_code) Values ('McClureberg', 'Jordan', '84709', '2855 Daniel Ranch', '0738');
