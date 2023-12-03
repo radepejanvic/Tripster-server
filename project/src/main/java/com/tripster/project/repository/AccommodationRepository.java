3aa2ec88a7bc0749d703a250c15c741afd03ba04
Rade Pejanović <radepejanovic@gmail.com>
2023-12-03 12:30:58 +0100

    feat: Created Amenity repo

--- project/src/main/java/com/tripster/project/repository/AccommodationRepository.java
+++ project/src/main/java/com/tripster/project/repository/AccommodationRepository.java
@@ -2,15 +2,11 @@ package com.tripster.project.repository;
 
 import com.tripster.project.model.Accommodation;
 import com.tripster.project.model.enums.AccommodationStatus;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.sql.Date;
-import java.util.Optional;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
