e7014085ab3c0ba0e0b5c5877369ea904361365a
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-03 13:11:15 +0100

    feat: Created LoginController

---
+++ project/src/main/java/com/tripster/project/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.tripster.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/login")
+public class LoginController {
+
+
+}
