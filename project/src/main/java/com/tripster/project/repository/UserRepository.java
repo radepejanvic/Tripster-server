bc5a621f6efc35d142eab4ac276a605c2fa8c55e
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-03 20:22:35 +0100

    feat: Renamed findByEmailAndPassword to findByEmail

--- project/src/main/java/com/tripster/project/repository/UserRepository.java
+++ project/src/main/java/com/tripster/project/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.tripster.project.repository;
 import com.tripster.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    public User findByEmailAndPassword (String email,String password);
+     Optional<User> findByEmail(String username);
 }
