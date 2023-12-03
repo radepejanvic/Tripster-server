371c9362782cbfff89746f482bf1951c13ba5ed4
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-03 20:19:48 +0100

    feat: Created function login

--- project/src/main/java/com/tripster/project/controller/LoginController.java
+++ project/src/main/java/com/tripster/project/controller/LoginController.java
@@ -1,5 +1,21 @@
 package com.tripster.project.controller;
 
+import com.tripster.project.dto.UserDTO;
+import com.tripster.project.mapper.UserDTOMapper;
+import com.tripster.project.model.User;
+import com.tripster.project.security.jwt.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +23,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/login")
 public class LoginController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping()
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO) {
+
+        User user = UserDTOMapper.fromUserDTOtoUser(userDTO);
+        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(user.getEmail(),
+                user.getPassword());
+        Authentication auth = authenticationManager.authenticate(authReq);
+
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userDTO.getEmail());
+        String token = jwtTokenUtil.generateToken(userDetails);
+
+
+        return new ResponseEntity<>(token, HttpStatus.OK);
+
+    }
 
 }
