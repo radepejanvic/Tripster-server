7d97e4899be8e31617d50b146e4cd83449978a92
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-03 20:29:11 +0100

    feat: Refactord code

--- project/src/main/java/com/tripster/project/service/UserServiceImpl.java
+++ project/src/main/java/com/tripster/project/service/UserServiceImpl.java
@@ -2,43 +2,22 @@ package com.tripster.project.service;
 
 import com.tripster.project.model.User;
 import com.tripster.project.repository.UserRepository;
-import com.tripster.project.service.interfaces.IUserService;
+import com.tripster.project.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
-import java.util.List;
-import java.util.function.Supplier;
 
 @Service
-public class UserServiceImpl implements IUserService {
+public class UserServiceImpl implements  UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    public User findOne(Long id) {return userRepository.findById(id).orElse(null);}
-
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public Page<User> findAll(Pageable page) {
-        return userRepository.findAll(page);
-    }
-
-    public User save(User User) {
-        return userRepository.save(User);
-    }
-
-    public void remove(Long id) {
-        userRepository.deleteById(id);
-    }
-
     @Override
-    public User findByEmailAndPassword(String email, String password) {
-        User user = userRepository.findByEmailAndPassword(email,password);
-        return user;
+    public User findOne(Long id) {
+        return userRepository.findById(id).orElse(null);
     }
-
 }
