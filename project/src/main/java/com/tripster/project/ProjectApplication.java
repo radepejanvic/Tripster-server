798e5dd34d7a34423406af7321b5f1d4fe6953e7
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-05 19:14:35 +0100

    feat: Refactored

--- project/src/main/java/com/tripster/project/ProjectApplication.java
+++ project/src/main/java/com/tripster/project/ProjectApplication.java
@@ -4,8 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 
 @SpringBootApplication
 public class ProjectApplication {
