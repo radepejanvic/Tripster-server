826df4a0cb49efb20d05a057b66f360b3add6e7b
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-05 19:00:55 +0100

    feat: Added updateStatus

--- project/src/main/java/com/tripster/project/repository/UserRepository.java
+++ project/src/main/java/com/tripster/project/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package com.tripster.project.repository;
 
 import com.tripster.project.model.User;
+import com.tripster.project.model.enums.UserStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    public User findByEmailAndPassword (String email,String password);
+    User findByEmailAndPassword (String email,String password);
+
+    @Transactional
+    @Modifying
+    @Query("update User u " +
+            "set u.status = :status " +
+            "where u.id = :id")
+    int updateStatus(Long id, UserStatus status);
+
 }
