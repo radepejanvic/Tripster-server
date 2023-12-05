95a073d96979ab0a2a2582534be891acfe05acd8
Rade Pejanović <radepejanovic@gmail.com>
2023-12-05 21:32:21 +0100

    feat: Created photo repository

---
+++ project/src/main/java/com/tripster/project/repository/PhotoRepository.java
@@ -0,0 +1,12 @@
+package com.tripster.project.repository;
+
+import com.tripster.project.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    List<Photo> findAllByAccommodationId(Long accommodationId);
+    
+}
