dcdd6145da715e0f28a9521081faf425074c5d04
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-05 11:19:42 +0100

    feat: Created interface ConfirmationTokenService

---
+++ project/src/main/java/com/tripster/project/service/interfaces/ConfirmationTokenService.java
@@ -0,0 +1,13 @@
+package com.tripster.project.service.interfaces;
+
+import com.tripster.project.model.ConfirmationToken;
+
+import java.util.Optional;
+
+public interface ConfirmationTokenService {
+    void save(ConfirmationToken confirmationToken);
+
+    Optional<ConfirmationToken> findByToken(String token);
+
+    int setConfirmedAt(String token);
+}
