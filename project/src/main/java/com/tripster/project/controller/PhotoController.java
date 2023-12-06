9f729023cd1990ccba76d5fb23de0bd698ed44dd
Rade Pejanović <radepejanovic@gmail.com>
2023-12-06 20:01:43 +0100

    feat: Created uploadPhotos endpoint

--- project/src/main/java/com/tripster/project/controller/PhotoController.java
+++ project/src/main/java/com/tripster/project/controller/PhotoController.java
@@ -1,12 +1,17 @@
 package com.tripster.project.controller;
 
+import com.tripster.project.model.Accommodation;
+import com.tripster.project.model.Photo;
+import com.tripster.project.service.AccommodationService;
 import com.tripster.project.service.interfaces.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,6 +20,9 @@ public class PhotoController {
 
     @Autowired
     private PhotoService photoService;
+
+    @Autowired
+    private AccommodationService accommodationService;
 
     @Value("${photos}")
     private String directory;
@@ -25,6 +33,20 @@ public class PhotoController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(photoService.findAllByAccommodationId(accommodationId));
+    }
+
+    @PostMapping("/{accommodationId}")
+    public ResponseEntity<?> uploadPhotos(@RequestParam("photo") MultipartFile[] photos, @PathVariable Long accommodationId) throws IOException {
+
+        Accommodation accommodation = accommodationService.findOne(accommodationId);
+        boolean hasPrimary = photoService.hasPrimary(accommodationId);
+
+        for (MultipartFile photoFile : photos) {
+            Photo photo = new Photo(!hasPrimary ? "primary" : "secondary", "jpg", accommodation);
+            photoService.save(photoFile, photo);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
 }
