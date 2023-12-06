21b7eaa88525655fd1475a2a6ba8edf48f81be27
Rade Pejanović <radepejanovic@gmail.com>
2023-12-06 19:02:13 +0100

    feat: Added findAllByAccommodationId

--- project/src/main/java/com/tripster/project/service/PhotoServiceImpl.java
+++ project/src/main/java/com/tripster/project/service/PhotoServiceImpl.java
@@ -4,9 +4,15 @@ import com.tripster.project.model.Photo;
 import com.tripster.project.repository.PhotoRepository;
 import com.tripster.project.service.interfaces.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PhotoServiceImpl implements PhotoService {
@@ -14,8 +20,23 @@ public class PhotoServiceImpl implements PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
-    public List<Photo> findByAccommodationId(Long accommodationId) {
-        return photoRepository.findByAccommodationId(accommodationId);
+    @Value(value="${photos}")
+    private String directory;
+
+    public List<byte[]> findAllByAccommodationId(Long accommodationId) {
+
+        List<byte[]> bytes = new ArrayList<>();
+
+        try {
+            for (Photo photo : photoRepository.findAllByAccommodationId(accommodationId)) {
+                bytes.add(Files.readAllBytes(new File(directory.concat(photo.getPath())).toPath()));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bytes;
     }
 
 }
--- project/src/main/java/com/tripster/project/service/interfaces/PhotoService.java
+++ project/src/main/java/com/tripster/project/service/interfaces/PhotoService.java
@@ -1,11 +1,12 @@
 package com.tripster.project.service.interfaces;
 
 import com.tripster.project.model.Photo;
+import org.springframework.core.io.InputStreamResource;
 
 import java.util.List;
 
 public interface PhotoService {
 
-    List<Photo> findByAccommodationId(Long accommodationId);
+    List<byte[]> findAllByAccommodationId(Long accommodationId);
 
 }
