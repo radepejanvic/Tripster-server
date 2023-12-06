b795a4e57fd7f9496df0cd69e82c93e8d34d8eba
Rade Pejanović <radepejanovic@gmail.com>
2023-12-06 20:00:38 +0100

    feat: Added save photos

--- project/src/main/java/com/tripster/project/service/PhotoServiceImpl.java
+++ project/src/main/java/com/tripster/project/service/PhotoServiceImpl.java
@@ -3,13 +3,17 @@ package com.tripster.project.service;
 import com.tripster.project.model.Photo;
 import com.tripster.project.repository.PhotoRepository;
 import com.tripster.project.service.interfaces.PhotoService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,7 +33,7 @@ public class PhotoServiceImpl implements PhotoService {
 
         try {
             for (Photo photo : photoRepository.findAllByAccommodationId(accommodationId)) {
-                bytes.add(Files.readAllBytes(new File(directory.concat(photo.getPath())).toPath()));
+                bytes.add(Files.readAllBytes(new File(directory.concat("/" + photo.getPath())).toPath()));
             }
 
         } catch (IOException e) {
@@ -37,6 +41,28 @@ public class PhotoServiceImpl implements PhotoService {
         }
 
         return bytes;
+    }
+
+    @Transactional
+    @Override
+    public void save(MultipartFile photoFile, Photo photo) throws IOException {
+//        StringBuilder builder = new StringBuilder();
+//        builder.append(directory).append("/").append(accommodationId).append("/").append(primary ? "primary" : "secondary").append("_").append("");
+        photoRepository.save(photo);
+
+        Path directoryPath = Path.of(directory);
+        Path filePath = directoryPath.resolve(photo.getPath());
+
+        if (!Files.exists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+        }
+
+        Files.copy(photoFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Override
+    public boolean hasPrimary(Long accommodationId) {
+        return photoRepository.hasPrimary(accommodationId) != 0;
     }
 
 }
--- project/src/main/java/com/tripster/project/service/interfaces/PhotoService.java
+++ project/src/main/java/com/tripster/project/service/interfaces/PhotoService.java
@@ -2,11 +2,17 @@ package com.tripster.project.service.interfaces;
 
 import com.tripster.project.model.Photo;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PhotoService {
 
     List<byte[]> findAllByAccommodationId(Long accommodationId);
+
+    void save(MultipartFile photoFile, Photo photo) throws IOException;
+
+    boolean hasPrimary(Long accommodationId);
 
 }
