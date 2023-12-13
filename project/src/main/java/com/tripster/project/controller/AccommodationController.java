c66f235e9139aa79bc48dffa2352fab66c0ab3dc
Rade Pejanović <radepejanovic@gmail.com>
2023-12-13 22:44:46 +0100

    fix: Amenity addition in put request

--- project/src/main/java/com/tripster/project/controller/AccommodationController.java
+++ project/src/main/java/com/tripster/project/controller/AccommodationController.java
@@ -132,6 +132,7 @@ public class AccommodationController {
 
         accommodation = AccommodationDTOMapper.fromDTOtoAccommodation(dto);
         accommodation.setOwner((Host)personService.findById(dto.getOwnerId()));
+        accommodation.setAmenities(amenityService.findByIdIn(dto.getAmenities()));
         accommodationService.save(accommodation);
 
         return new ResponseEntity<>(dto, HttpStatus.OK);
