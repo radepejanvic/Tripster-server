7147d86764cf1cfda83d2efbff31b854f5d557fc
Rade Pejanović <radepejanovic@gmail.com>
2023-12-15 00:32:05 +0100

    fix: Added all args constructor

--- project/src/main/java/com/tripster/project/dto/PriceDTO.java
+++ project/src/main/java/com/tripster/project/dto/PriceDTO.java
@@ -1,5 +1,6 @@
 package com.tripster.project.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PriceDTO {
 
     private LocalDate start;
