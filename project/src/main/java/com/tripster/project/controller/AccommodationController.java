f7de5378416268c4db4bd2195cfbc19550aa4413
Rade Pejanović <radepejanovic@gmail.com>
2023-12-14 01:54:09 +0100

    feat: Added calendar put post and get endpoints

--- project/src/main/java/com/tripster/project/controller/AccommodationController.java
+++ project/src/main/java/com/tripster/project/controller/AccommodationController.java
@@ -8,6 +8,7 @@ import com.tripster.project.model.enums.AccommodationStatus;
 import com.tripster.project.model.enums.AccommodationType;
 import com.tripster.project.service.AccommodationService;
 import com.tripster.project.service.AmenityService;
+import com.tripster.project.service.CalendarService;
 import com.tripster.project.service.interfaces.IPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -35,6 +36,9 @@ public class AccommodationController {
     @Qualifier("hostServiceImpl")
     @Autowired
     private IPersonService personService;
+
+    @Autowired
+    private CalendarService calendarService;
 
 
     // Admin: when he opens the page for accommodation approval
@@ -114,7 +118,7 @@ public class AccommodationController {
         accommodation.setOwner((Host)personService.findById(dto.getOwnerId()));
         accommodation.setAmenities(amenityService.findByIdIn(dto.getAmenities()));
 
-        accommodationService.generateCalendar(LocalDate.now(), accommodation);
+//        accommodationService.generateCalendar(LocalDate.now(), accommodation);
 
         accommodationService.save(accommodation);
 
@@ -138,12 +142,31 @@ public class AccommodationController {
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/price/{accommodationId}", consumes = "application/json")
+    public ResponseEntity<Integer> addCalendar(@PathVariable Long accommodationId, @RequestBody List<PriceDTO> dtos) {
+
+        Accommodation accommodation = accommodationService.findOne(accommodationId);
+        accommodation.setCalendar(calendarService.getCalendar(dtos));
+        accommodationService.save(accommodation);
+
+        return new ResponseEntity<>(
+                accommodation.getCalendar().size(), HttpStatus.OK);
+    }
+
     @PutMapping(value = "/price/{accommodationId}", consumes = "application/json")
-    public ResponseEntity<?> updateAccommodation(@PathVariable Long accommodationId, @RequestBody List<PriceDTO> dtos) {
+    public ResponseEntity<Integer> updateAccommodation(@PathVariable Long accommodationId, @RequestBody List<PriceDTO> dtos) {
 
+        int length = calendarService.updateCalendar(accommodationId, dtos);
 
+        return new ResponseEntity<>(length, HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping(value = "calendar/{id}")
+    public ResponseEntity<?> getCalendar(@PathVariable Long id) {
+
+        Accommodation accommodation = accommodationService.findOne(id);
+
+        return new ResponseEntity<>(accommodation.getCalendar(), HttpStatus.OK);
     }
 
     @PatchMapping(consumes = "application/json")
