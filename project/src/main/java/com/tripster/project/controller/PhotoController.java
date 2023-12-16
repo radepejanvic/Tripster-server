a2c3d10b9e1fd3527b528a238df90144b38b8a68
Rade Pejanović <radepejanovic@gmail.com>
2023-12-16 15:16:41 +0100

    fix: Photo naming issue

--- project/src/main/java/com/tripster/project/controller/PhotoController.java
+++ project/src/main/java/com/tripster/project/controller/PhotoController.java
@@ -43,6 +43,7 @@ public class PhotoController {
 
         for (MultipartFile photoFile : photos) {
             Photo photo = new Photo(!hasPrimary ? "primary" : "secondary", "jpg", accommodation);
+            hasPrimary = false;
             photoService.save(photoFile, photo);
         }
 
