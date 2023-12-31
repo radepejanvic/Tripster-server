768ef7805ffea1095b8e0e2957ca90b83f98d9fb
Rade Pejanović <radepejanovic@gmail.com>
2023-12-31 16:09:12 +0100

    feat: Added canReview functions

---
+++ project/src/main/java/com/tripster/project/service/ReviewService.java
@@ -0,0 +1,30 @@
+package com.tripster.project.service;
+
+import com.tripster.project.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    public boolean canReviewHost(Long hostId, Long guestId) {
+
+        LocalDate today = LocalDate.now();
+
+        return reservationRepository.findAllByGuestAndHost(hostId, guestId, today) != 0;
+    }
+
+    public boolean canReviewAccommodation(Long accommodationId, Long guestId) {
+
+        LocalDate today = LocalDate.now();
+        LocalDate pastSevenDays = today.minusDays(7);
+
+        return reservationRepository.findAllByGuestAndAccommodation(accommodationId, guestId, today, pastSevenDays) != 0;
+    }
+
+}
