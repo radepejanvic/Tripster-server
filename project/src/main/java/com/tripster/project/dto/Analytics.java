51733c7aeda170a06d63b00715c7a9f329217e10
Rade Pejanović <radepejanovic@gmail.com>
2024-01-05 20:18:19 +0100

    feat: Adapted constructor

---
+++ project/src/main/java/com/tripster/project/dto/Analytics.java
@@ -0,0 +1,25 @@
+package com.tripster.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+
+@Getter
+@Setter
+public class Analytics {
+
+    public long[] reservationsPerMonth;
+    public double[] revenuePerMonth;
+    private long totalReservations;
+    private double totalRevenue;
+
+    public Analytics() {
+        reservationsPerMonth = new long[12];
+        Arrays.fill(reservationsPerMonth, 0);
+
+        revenuePerMonth = new double[12];
+        Arrays.fill(revenuePerMonth, 0);
+    }
+
+}
