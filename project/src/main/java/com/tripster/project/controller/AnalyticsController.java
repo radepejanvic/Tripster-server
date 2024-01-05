daab60082ef884479f7d37442dc196a4859da3d3
Rade Pejanović <radepejanovic@gmail.com>
2024-01-05 20:18:56 +0100

    feat: Added annual analytics endpoint

---
+++ project/src/main/java/com/tripster/project/controller/AnalyticsController.java
@@ -0,0 +1,38 @@
+package com.tripster.project.controller;
+
+import com.tripster.project.dto.AccommodationReviewReportDTO;
+import com.tripster.project.dto.Analytics;
+import com.tripster.project.mapper.ReviewReportDTOMapper;
+import com.tripster.project.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/analytics")
+public class AnalyticsController {
+
+    @Autowired
+    private AnalyticsService analyticsService;
+
+//    @PreAuthorize("hasRole('HOST')")
+    @GetMapping(value = "/{id}/{year}")
+    public ResponseEntity<List<Analytics>> getAnnualAnalytics(@PathVariable Long id, @PathVariable int year) {
+
+        return new ResponseEntity<>(analyticsService.generateAnnualAnalytics(id, year), HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/{id}/{year}")
+//    public ResponseEntity<List<Object[]>> gen(@PathVariable Long id, @PathVariable int year) {
+//
+//        return new ResponseEntity<>(analyticsService.gen(id, year), HttpStatus.OK);
+//    }
+
+}
