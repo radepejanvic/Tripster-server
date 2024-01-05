cb57588ffdaee8a53763775a65729af8b469db33
Rade Pejanović <radepejanovic@gmail.com>
2024-01-05 18:05:47 +0100

    feat: Created analytics service

---
+++ project/src/main/java/com/tripster/project/service/AnalyticsService.java
@@ -0,0 +1,14 @@
+package com.tripster.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsService {
+
+    @Autowired
+    private ReservationServiceImpl reservationService;
+
+//    public generateAnnualAnalytics()
+
+}
