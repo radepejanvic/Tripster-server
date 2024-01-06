d649c35faa468713352a366bb09f7595917437da
Rade Pejanović <radepejanovic@gmail.com>
2024-01-06 19:37:07 +0100

    feat: Added generate status analytics

--- project/src/main/java/com/tripster/project/service/AnalyticsService.java
+++ project/src/main/java/com/tripster/project/service/AnalyticsService.java
@@ -1,10 +1,15 @@
 package com.tripster.project.service;
 
 import com.tripster.project.dto.Analytics;
+import com.tripster.project.dto.StatusAnalytics;
+import com.tripster.project.model.enums.ReservationStatus;
 import com.tripster.project.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,5 +56,45 @@ public class AnalyticsService {
         analytic.reservationsPerMonth[month-1] = count;
         analytic.revenuePerMonth[month-1] = revenue;
     }
+
+    public StatusAnalytics generateStatusAnalytics(Long hostId, long startMillis, long endMillis) {
+        StatusAnalytics analytics = new StatusAnalytics();
+        LocalDate start = Instant.ofEpochMilli(startMillis).atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate end = Instant.ofEpochMilli(endMillis).atZone(ZoneId.systemDefault()).toLocalDate();
+
+
+        List<Object[]> raw = reservationRepository.calculateTotalAnalyticsPerStatus(hostId, start, end);
+
+        for (Object[] o : raw) {
+            setStats(analytics, o);
+        }
+
+        return analytics;
+    }
+
+    private void setStats(StatusAnalytics analytic, Object[] stats) {
+        ReservationStatus status = (ReservationStatus) stats[0];
+        long count = (long) stats[1];
+        double revenue = (double) stats[2];
+
+        switch(status) {
+            case PENDING:
+                analytic.setPendingCount(count);
+                analytic.setPendingRevenue(revenue);
+                return;
+            case ACCEPTED:
+                analytic.setAcceptedCount(count);
+                analytic.setAcceptedRevenue(revenue);
+                return;
+            case CANCELLED:
+                analytic.setCancelledCount(count);
+                analytic.setCancelledRevenue(revenue);
+                return;
+            case REJECTED:
+                analytic.setRejectedCount(count);
+                analytic.setRejectedRevenue(revenue);
+        }
+    }
+
 
 }
