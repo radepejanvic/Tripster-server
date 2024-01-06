dae90f048387121022f4f19142ed0b94d6671560
Rade Pejanović <radepejanovic@gmail.com>
2024-01-06 20:15:18 +0100

    feat: Added generateCustomPeriodAnalytics

--- project/src/main/java/com/tripster/project/service/AnalyticsService.java
+++ project/src/main/java/com/tripster/project/service/AnalyticsService.java
@@ -20,8 +20,9 @@ public class AnalyticsService {
     private ReservationRepository reservationRepository;
 
     public List<Analytics> generateAnnualAnalytics(Long hostId, int year) {
-        List<Object[]> raw = reservationRepository.calculateAnnualAnalytics(hostId, year);
         List<Analytics> analytics = new ArrayList<>();
+
+        List<Object[]> raw = reservationRepository.calculateAnnualAnalytics(hostId, year);
 
         Long currentId = (Long) raw.get(0)[0];
         Analytics analytic = new Analytics();
@@ -35,7 +36,7 @@ public class AnalyticsService {
                 analytic = new Analytics();
             }
 
-            setStats(analytic, o);
+            mapStatsToAnnualAnalytics(analytic, o);
         }
 
         analytics.add(analytic);
@@ -47,7 +48,7 @@ public class AnalyticsService {
         return reservationRepository.calculateAnnualAnalytics(hostId, year);
     }
 
-    private void setStats(Analytics analytic, Object[] stats) {
+    private void mapStatsToAnnualAnalytics(Analytics analytic, Object[] stats) {
         int month = (int) stats[2];
         long count = (long) stats[4];
         double revenue = (double) stats[5];
@@ -62,17 +63,16 @@ public class AnalyticsService {
         LocalDate start = Instant.ofEpochMilli(startMillis).atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate end = Instant.ofEpochMilli(endMillis).atZone(ZoneId.systemDefault()).toLocalDate();
 
-
         List<Object[]> raw = reservationRepository.calculateTotalAnalyticsPerStatus(hostId, start, end);
 
-        for (Object[] o : raw) {
-            setStats(analytics, o);
+        for (Object[] stats : raw) {
+            mapStatsToStatusAnalytics(analytics, stats);
         }
 
         return analytics;
     }
 
-    private void setStats(StatusAnalytics analytic, Object[] stats) {
+    private void mapStatsToStatusAnalytics(StatusAnalytics analytic, Object[] stats) {
         ReservationStatus status = (ReservationStatus) stats[0];
         long count = (long) stats[1];
         double revenue = (double) stats[2];
@@ -96,5 +96,29 @@ public class AnalyticsService {
         }
     }
 
+    public List<Analytics> generateCustomPeriodAnalytics(Long hostId, long startMillis, long endMillis) {
+        List<Analytics> analytics = new ArrayList<>();
+        LocalDate start = Instant.ofEpochMilli(startMillis).atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate end = Instant.ofEpochMilli(endMillis).atZone(ZoneId.systemDefault()).toLocalDate();
+
+        List<Object[]> raw = reservationRepository.calculateTotalAnalyticsPerAccommodation(hostId, start, end);
+
+        for (Object[] stats : raw) {
+            analytics.add(mapStatsToTotalAnalytics(stats));
+        }
+
+        return analytics;
+    }
+    
+    private Analytics mapStatsToTotalAnalytics(Object[] stats) {
+        Analytics analytic = new Analytics();
+
+        analytic.setAccommodationId((long) stats[0]);
+        analytic.setName((String) stats[1]);
+        analytic.setTotalReservations((long) stats[2]);
+        analytic.setTotalRevenue((double) stats[3]);
+
+        return analytic;
+    }
 
 }
