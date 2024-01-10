c98f2562a5b6909587d089e5742188691d34ec6a
Rade Pejanović <radepejanovic@gmail.com>
2024-01-10 18:41:11 +0100

    feat: Added title to notification entity

--- project/src/main/java/com/tripster/project/model/Notification.java
+++ project/src/main/java/com/tripster/project/model/Notification.java
@@ -17,6 +17,9 @@ public class Notification {
     private Long id;
 
     @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
     private String text;
 
     @Enumerated(EnumType.STRING)
--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -61,16 +61,16 @@ insert into reservation (start_date, end_date, duration, price, guests_no, guest
 insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status, deleted) values ('2023-08-13', '2023-08-14', 1, 30.0, 3, 3, 2, 'PENDING', false);
 insert into reservation (start_date, end_date, duration, price, guests_no, guest_id, accommodation_id, status, deleted) values ('2023-08-12', '2023-08-17', 5, 100.0, 4, 4, 3, 'PENDING', false);
 
-insert into notification (user_id, text, status) values (1, 'Exc of accessory spleen', 'SENT');
-insert into notification (user_id, text, status) values (1, 'Remove penetrat FB eye', 'READ');
-insert into notification (user_id, text, status) values (1, 'Other suture of tendon', 'READ');
-insert into notification (user_id, text, status) values (2, 'Remove penetrat cerv FB', 'SENT');
-insert into notification (user_id, text, status) values (2, 'Pacemaker impedance chck', 'SENT');
-insert into notification (user_id, text, status) values (3, 'Open reduc-dislocat NEC', 'READ');
-insert into notification (user_id, text, status) values (4, 'Arth/pros rem wo rep NOS', 'READ');
-insert into notification (user_id, text, status) values (5, 'Failed forceps', 'NEW');
-insert into notification (user_id, text, status) values (5, 'Disarticulation of wrist', 'SENT');
-insert into notification (user_id, text, status) values (6, 'Ligate thoracic duct', 'SENT');
+insert into notification (user_id, title, text, status) values (1, 'Title', 'Exc of accessory spleen', 'SENT');
+insert into notification (user_id, title, text, status) values (1, 'Title', 'Remove penetrat FB eye', 'READ');
+insert into notification (user_id, title, text, status) values (1, 'Title', 'Other suture of tendon', 'READ');
+insert into notification (user_id, title, text, status) values (2, 'Title', 'Remove penetrat cerv FB', 'SENT');
+insert into notification (user_id, title, text, status) values (2, 'Title', 'Pacemaker impedance chck', 'SENT');
+insert into notification (user_id, title, text, status) values (3, 'Title', 'Open reduc-dislocat NEC', 'READ');
+insert into notification (user_id, title, text, status) values (4, 'Title', 'Arth/pros rem wo rep NOS', 'READ');
+insert into notification (user_id, title, text, status) values (5, 'Title', 'Failed forceps', 'NEW');
+insert into notification (user_id, title, text, status) values (5, 'Title', 'Disarticulation of wrist', 'SENT');
+insert into notification (user_id, title, text, status) values (6, 'Title', 'Ligate thoracic duct', 'SENT');
 
 insert into user_review (reviewer_id, title, reviewed_user_id, comment, rate, status) values (1, 'This is my title', 2, 'Incise cerebral meninges', 1, 'REPORTED');
 insert into user_review (reviewer_id, title, reviewed_user_id, comment, rate, status) values (2, 'This is my title', 1, 'Extracorpor hepat Assis', 2, 'REPORTED');
