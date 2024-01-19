56b0df3f22a84e06a737ee3322f8c455f57993d5
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-19 16:00:49 +0100

    feat: Created CalendarServiceTest

---
+++ project/src/test/java/com/tripster/project/service/CalendarServiceTest.java
@@ -0,0 +1,23 @@
+package com.tripster.project.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class CalendarServiceTest {
+
+    @MockBean
+    private AccommodationService accommodationService;
+
+    @Autowired
+    private CalendarService calendarService;
+
+
+}
