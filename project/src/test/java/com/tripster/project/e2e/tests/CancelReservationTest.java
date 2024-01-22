7e722a0394eb6c011e489a5e24a342ce43357bef
Rade Pejanović <radepejanovic@gmail.com>
2024-01-22 14:48:54 +0100

    fix: Fixed data and cancel reservation test

--- project/src/test/java/com/tripster/project/e2e/tests/CancelReservationTest.java
+++ project/src/test/java/com/tripster/project/e2e/tests/CancelReservationTest.java
@@ -2,20 +2,21 @@ package com.tripster.project.e2e.tests;
 
 import com.tripster.project.e2e.pages.GuestReservationsPage;
 import com.tripster.project.e2e.pages.HomePage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CancelReservationTest extends TestBase {
 
-    private  static final String USERNAME = "guest@hotmail.com";
-    private  static final String PASSWORD = "guest";
+    private  static final String USERNAME = "test@gmail.com";
+    private  static final String PASSWORD = "admin";
     private  static final String ACCOMMODATION_NAME = "Reilly-Volkman";
-    private  static final String TIMESTAMP = " 22.07.2024. - 26.07.2024. ";
+    private  static final String TIMESTAMP = " 22.07.2025. - 26.07.2025. ";
     private  static final String STATUS = "CANCELLED";
 
     @Test
+    @DisplayName("Cancel reservation")
     void test_cancel_reservation() {
         HomePage homePage = new HomePage(driver);
         homePage.login(USERNAME,PASSWORD);
